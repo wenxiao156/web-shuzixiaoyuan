8d74dc66a552d88ffe3c69206945bd7b8c3839f0
wenxiao123 <821953863@qq.com>
2018-06-12 16:29:55 +0800

    首页

--- widget/html/main/index.js
+++ widget/html/main/index.js
@@ -5,22 +5,27 @@ define(function (require, exports, module) {
         el: '#main',
         template: _g.getTemplate('main/index_view'),
         data: {
-           indexList: [{
-            icon: '../../image/main/lecture.png',
-            title: '讲座',
-            url: '../../html/lecture/list_frame.html'
-           },{
-            icon: '../../image/main/conversation.png',
-            title: '茶座',
-            url: '../../html/conversation/list_frame.html'
-           },{
-            icon: '../../image/main/me.png',
-            title: '个人',
-            url: '../../html/lecture/list_frame.html'
-           },{
-            icon: '../../image/main/message.png',
-            title: '通知',
-            url: '../../html/lecture/list_frame.html'
+            userName: 'Y丶yin',
+            messageNum: 2,
+            registration: 1,
+            lectureRecord: 32,
+            conversationRecord: 21,
+            indexList: [{
+                // icon: '../../image/main/lecture.png',
+                title: '讲座',
+                url: '../../html/lecture/list_frame.html'
+            },{
+                // icon: '../../image/main/conversation.png',
+                title: '茶座',
+                url: '../../html/conversation/list_frame.html'
+            },{
+                // icon: '../../image/main/me.png',
+                title: '参与记录',
+                url: '../../html/lecture/list_frame.html'
+            },{
+                // icon: '../../image/main/me.png',
+                title: '提问',
+                url: '../../html/lecture/list_frame.html'
            }] 
         },
         ready: function() {
